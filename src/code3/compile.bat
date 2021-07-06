1ac5df7ef7da3a0a8dd463f49e17e96106bb78c6
ThatOSDev <starlordgo@protonmail.com>
2021-07-06 08:23:14 -0400

    Trying to find bug

--- src/code3/compile.bat
+++ src/code3/compile.bat
@@ -1,3 +1,3 @@
-gcc -Wall -Werror -m64 -masm=intel -mabi=ms -ffreestanding -Wl,--oformat=binary -e main -c loader.c -o loader.bin
+gcc -Wall -Werror -m64 -ffreestanding -Wl,--oformat=binary -e main -c loader.c -o loader.bin
 
 pause
--- src/code3/loader.c
+++ src/code3/loader.c
@@ -1,46 +1,52 @@
 // Operating System from Scratch Tutorials - ThatOSDev ( 2021 )
 // https://github.com/ThatOSDev/ThatOS64
 
-#include "stdint.h"
-#include "tosdfont.h"
-
-             // ARGB             
 #define ORANGE 0xffffa500
 #define CYAN   0xff00ffff
-#define RED    0xffff0000
-#define GREEN  0xff00ff00
-#define BLUE   0xff0000ff
-#define GRAY   0xff888888
-#define WHITE  0xffffffff
-#define BLACK  0xff000000
 
-#define CHARBITS  128
-#define FONTSIZE8 8
-#define ZERO      0x0
+unsigned int asciifont[32768] = {
+
+1,0,0,0,0,0,0,0, // 000  --  NUL 
+1,0,0,1,0,0,0,0,
+1,1,0,1,0,0,0,0,
+1,0,1,1,0,0,0,0,
+1,0,0,1,0,0,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,1,0,1,0,0,
+0,0,0,1,0,1,0,0,
+0,0,0,1,0,1,0,0,
+0,0,0,1,1,1,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,1,0,0,
+0,0,0,0,0,1,0,0,
+0,0,0,0,0,1,0,0,
+0,0,0,0,0,1,1,1,
+0,0,0,0,0,0,0,1
+};
 
 typedef struct GRAPHICS_COLOR_PIXEL
 {
-    uint8_t   Blue;
-    uint8_t   Green;
-    uint8_t   Red;
-    uint8_t   Alpha;
+    unsigned char   Blue;
+    unsigned char   Green;
+    unsigned char   Red;
+    unsigned char   Alpha;
 } GRAPHICS_COLOR_PIXEL;
 
 typedef struct BLOCKINFO
 {
-	int32_t    MagicNumber;
-    uint64_t*  BaseAddress;
-    uint64_t   BufferSize;
-    uint32_t   ScreenWidth;
-    uint32_t   ScreenHeight;
-    uint32_t   PixelsPerScanLine;
+	int    MagicNumber;
+    unsigned long long*  BaseAddress;
+    unsigned long long   BufferSize;
+    unsigned int   ScreenWidth;
+    unsigned int   ScreenHeight;
+    unsigned int   PixelsPerScanLine;
 } BLOCKINFO;
 
-GRAPHICS_COLOR_PIXEL* SetGraphicsColor(uint32_t color);
-void CreateBufferFilledBox(uint32_t xPos, uint32_t yPos, uint32_t w, uint32_t h, GRAPHICS_COLOR_PIXEL* gc, BLOCKINFO* bli);
-void GetCharacter(uint32_t character, uint32_t xPos, uint32_t yPos, BLOCKINFO* block);
+GRAPHICS_COLOR_PIXEL* SetGraphicsColor(unsigned int color);
+void CreateBufferFilledBox(unsigned int xPos, unsigned int yPos, unsigned int w, unsigned int h, GRAPHICS_COLOR_PIXEL* gc, BLOCKINFO* bli);
+void GetCharacter(unsigned int xPos, unsigned int yPos, BLOCKINFO* block);
 
-void main(int32_t argc, BLOCKINFO* bi)
+void main(int argc, BLOCKINFO* bi)
 {
 	BLOCKINFO* biStruct = bi;
 	
@@ -49,13 +55,12 @@ void main(int32_t argc, BLOCKINFO* bi)
 		GRAPHICS_COLOR_PIXEL* GraphicsColor = SetGraphicsColor(ORANGE);
 		CreateBufferFilledBox(0, 0, 150, 150, GraphicsColor, biStruct);
 	}
-	
-	GetCharacter(0, 0, 0, biStruct);
+	GetCharacter(0, 0, biStruct);
 	
 	while(1){}
 }
 
-GRAPHICS_COLOR_PIXEL* SetGraphicsColor(uint32_t color)
+GRAPHICS_COLOR_PIXEL* SetGraphicsColor(unsigned int color)
 {
     GRAPHICS_COLOR_PIXEL* GColor = ((void*)0);
     GColor->Alpha    = color >> 24;
@@ -65,42 +70,42 @@ GRAPHICS_COLOR_PIXEL* SetGraphicsColor(uint32_t color)
     return GColor;
 }
 
-void CreateBufferFilledBox(uint32_t xPos, uint32_t yPos, uint32_t w, uint32_t h, GRAPHICS_COLOR_PIXEL* gc, BLOCKINFO* bli)
+void CreateBufferFilledBox(unsigned int xPos, unsigned int yPos, unsigned int w, unsigned int h, GRAPHICS_COLOR_PIXEL* gc, BLOCKINFO* bli)
 {
-    uint32_t x;
-    uint32_t y      = yPos;
-    uint32_t width  = xPos + w;
-    uint32_t height = yPos + h;
+    unsigned int x;
+    unsigned int y      = yPos;
+    unsigned int width  = xPos + w;
+    unsigned int height = yPos + h;
 
     for(y = yPos; y < height; y++)
     {
         for(x = xPos; x < width; x++)
         {
-            *(x + (y * bli->PixelsPerScanLine) + (uint32_t*)(bli->BaseAddress)) = *(uint32_t*)gc;
+            *(x + (y * bli->PixelsPerScanLine) + (unsigned int*)(bli->BaseAddress)) = *(unsigned int*)gc;
         }
     }
 }
 
-void GetCharacter(uint32_t character, uint32_t xPos, uint32_t yPos, BLOCKINFO* block)
+void GetCharacter(unsigned int xPos, unsigned int yPos, BLOCKINFO* block)
 {
-    uint32_t initPos = xPos;
-	uint32_t mcX = initPos;
-	uint32_t mcY = yPos;
-	uint32_t xCounter = ZERO;
-	uint32_t yCounter = ZERO;
-	uint32_t fontsz   = FONTSIZE8;
+    unsigned int initPos  = xPos;
+	unsigned int mcX      = initPos;
+	unsigned int mcY      = yPos;
+	unsigned int xCounter = 0;
+	unsigned int yCounter = 0;
+	unsigned int fontsz   = 8;
 
-	for(uint32_t t = (character * CHARBITS); t < ((character * CHARBITS) + CHARBITS); t++)
+	for(unsigned int t = 0; t < 128; t++)
 	{
 		if(xCounter > 7)
 		{
-			xCounter = ZERO;
+			xCounter = 0;
 			mcY += fontsz;
 			mcX = initPos;
 			yCounter++;
 			if(yCounter > 15)
 			{
-				yCounter = ZERO;
+				yCounter = 0;
 				mcY += fontsz;
 			}
 		}
@@ -113,7 +118,7 @@ void GetCharacter(uint32_t character, uint32_t xPos, uint32_t yPos, BLOCKINFO* b
 			}
 			case 1:
 			{
-				CreateBufferFilledBox(mcX, mcY, fontsz, fontsz, SetGraphicsColor(WHITE), block);
+				CreateBufferFilledBox(mcX, mcY, fontsz, fontsz, SetGraphicsColor(CYAN), block);
 				break;
 			}
 			default:
