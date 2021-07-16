56d5c0a178633e34771bf563918b094819beace9
ThatOSDev <starlordgo@protonmail.com>
2021-07-16 10:23:42 -0400

    Updating EFI File

--- src/code3/compile.bat
+++ src/code3/compile.bat
@@ -1,3 +1,3 @@
-gcc -Wall -Werror -m64 -ffreestanding -Wl,--oformat=binary -e main -c loader.c -o loader.bin
+gcc -ansi -masm=intel -std=c99  -pedantic-errors -Wall -Werror -m64 -nostdinc -nostdlib -ffreestanding -Wl,--oformat=binary -e main -c loader.c -o loader.bin
 
 pause
--- src/code3/loader.c
+++ src/code3/loader.c
@@ -1,133 +1,33 @@
 // Operating System from Scratch Tutorials - ThatOSDev ( 2021 )
 // https://github.com/ThatOSDev/ThatOS64
 
-#define ORANGE 0xffffa500
-#define CYAN   0xff00ffff
-
-unsigned int asciifont[128] = {
-
-1,0,0,0,0,0,0,0, // 000  --  NUL 
-1,0,0,1,0,0,0,0,
-1,1,0,1,0,0,0,0,
-1,0,1,1,0,0,0,0,
-1,0,0,1,0,0,0,0,
-0,0,0,0,0,0,0,0,
-0,0,0,1,0,1,0,0,
-0,0,0,1,0,1,0,0,
-0,0,0,1,0,1,0,0,
-0,0,0,1,1,1,0,0,
-0,0,0,0,0,0,0,0,
-0,0,0,0,0,1,0,0,
-0,0,0,0,0,1,0,0,
-0,0,0,0,0,1,0,0,
-0,0,0,0,0,1,1,1,
-0,0,0,0,0,0,0,1
-};
-
-typedef struct GRAPHICS_COLOR_PIXEL
+// From the UEFI PDF Page 170
+typedef struct EFI_MEMORY_DESCRIPTOR
 {
-    unsigned char   Blue;
-    unsigned char   Green;
-    unsigned char   Red;
-    unsigned char   Alpha;
-} GRAPHICS_COLOR_PIXEL;
+	unsigned int        type;
+	void*               PhysicalAddress;
+	void*               VirtualAddress;
+	unsigned long long  NumberOfPages;
+	unsigned long long  Attributes;
+} EFI_MEMORY_DESCRIPTOR;
 
 typedef struct BLOCKINFO
 {
-    int                  MagicNumber;
-    unsigned long long*  BaseAddress;
-    unsigned long long   BufferSize;
-    unsigned int         ScreenWidth;
-    unsigned int         ScreenHeight;
-    unsigned int         PixelsPerScanLine;
+    unsigned long long*     BaseAddress;
+    unsigned long long      BufferSize;
+    unsigned int            ScreenWidth;
+    unsigned int            ScreenHeight;
+    unsigned int            PixelsPerScanLine;
+	unsigned long long*     LoaderFileSize;
+	EFI_MEMORY_DESCRIPTOR*  MMap;
+	unsigned long long      MMapSize;
+	unsigned long long      MMapDescriptorSize;
 } BLOCKINFO;
 
-GRAPHICS_COLOR_PIXEL* SetGraphicsColor(unsigned int color);
-void CreateBufferFilledBox(unsigned int xPos, unsigned int yPos, unsigned int w, unsigned int h, GRAPHICS_COLOR_PIXEL* gc, BLOCKINFO* bli);
-void GetCharacter(unsigned int xPos, unsigned int yPos, BLOCKINFO* block);
-
-void main(int argc, BLOCKINFO* bi)
+void main(BLOCKINFO* bi)
 {
-	BLOCKINFO* biStruct = bi;
-	
-	if(biStruct->MagicNumber == 31337)
-	{
-		GRAPHICS_COLOR_PIXEL* GraphicsColor = SetGraphicsColor(ORANGE);
-		CreateBufferFilledBox(0, 0, 150, 150, GraphicsColor, biStruct);
-	}
-	GetCharacter(0, 0, biStruct);
-	
-	while(1){}
+	*(unsigned int*)(20 + (20 * bi->PixelsPerScanLine) + (unsigned int*)(bi->BaseAddress)) = 0xffffa500;
+	while(1){__asm__ ("hlt");}
 }
 
-GRAPHICS_COLOR_PIXEL* SetGraphicsColor(unsigned int color)
-{
-    GRAPHICS_COLOR_PIXEL* GColor = ((void*)0);
-    GColor->Alpha    = color >> 24;
-    GColor->Red      = color >> 16;
-    GColor->Green    = color >> 8;
-    GColor->Blue     = color;
-    return GColor;
-}
-
-void CreateBufferFilledBox(unsigned int xPos, unsigned int yPos, unsigned int w, unsigned int h, GRAPHICS_COLOR_PIXEL* gc, BLOCKINFO* bli)
-{
-    unsigned int x;
-    unsigned int y      = yPos;
-    unsigned int width  = xPos + w;
-    unsigned int height = yPos + h;
-
-    for(y = yPos; y < height; y++)
-    {
-        for(x = xPos; x < width; x++)
-        {
-            *(x + (y * bli->PixelsPerScanLine) + (unsigned int*)(bli->BaseAddress)) = *(unsigned int*)gc;
-        }
-    }
-}
-
-void GetCharacter(unsigned int xPos, unsigned int yPos, BLOCKINFO* block)
-{
-    unsigned int initPos  = xPos;
-    unsigned int mcX      = initPos;
-    unsigned int mcY      = yPos;
-    unsigned int xCounter = 0;
-    unsigned int yCounter = 0;
-    unsigned int fontsz   = 8;
-
-	for(unsigned int t = 0; t < 128; t++)
-	{
-		if(xCounter > 7)
-		{
-			xCounter = 0;
-			mcY += fontsz;
-			mcX = initPos;
-			yCounter++;
-			if(yCounter > 15)
-			{
-				yCounter = 0;
-				mcY += fontsz;
-			}
-		}
-
-		switch(asciifont[t])
-		{
-			case 0:
-			{
-				break;
-			}
-			case 1:
-			{
-				CreateBufferFilledBox(mcX, mcY, fontsz, fontsz, SetGraphicsColor(CYAN), block);
-				break;
-			}
-			default:
-			{
-				break;
-			}
-		}
-		mcX += fontsz;
-		xCounter++;
-	}
-}
-
+// __asm__ __volatile__
