7d73456207fc8d1935e2cec44ae501df6a6015e9
ThatOSDev <starlordgo@protonmail.com>
2021-07-17 12:14:23 -0400

    Added No Optimization switch

--- src/code3/compile.bat
+++ src/code3/compile.bat
@@ -1,3 +1,3 @@
-gcc -ansi -masm=intel -std=c99  -pedantic-errors -Wall -Werror -m64 -nostdinc -nostdlib -ffreestanding -Wl,--oformat=binary -e main -c loader.c -o loader.bin
+gcc -ansi -masm=intel -std=c99 -O0 -pedantic-errors -Wall -Werror -Wno-unused-variable -m64 -nostdinc -nostdlib -ffreestanding -Wl,--oformat=binary -e main -c loader.c -o loader.bin
 
 pause
--- src/code3/loader.c
+++ src/code3/loader.c
@@ -1,8 +1,16 @@
 // Operating System from Scratch Tutorials - ThatOSDev ( 2021 )
 // https://github.com/ThatOSDev/ThatOS64
 
-#define ORANGE 0xffffa500
-#define CYAN   0xff00ffff
+                  // A R G B             
+#define ORANGE     0xffffa500
+#define DARKORANGE 0xffaf5500
+#define CYAN       0xff00ffff
+#define RED        0xffff0000
+#define GREEN      0xff00ff00
+#define BLUE       0xff0000ff
+#define GRAY       0xff888888
+#define WHITE      0xffffffff
+#define BLACK      0xff000000
 
 unsigned int asciifont[128] = {
 
@@ -47,22 +55,30 @@ typedef struct BLOCKINFO
 	unsigned long long      MMapDescriptorSize;
 } BLOCKINFO;
 
-void SetCharacter(unsigned int a, unsigned int b, BLOCKINFO* bli, unsigned int c, unsigned int FontSize);
-void MakeRectangle(unsigned int a, unsigned int b, unsigned int w, unsigned int h, BLOCKINFO* bli, unsigned int c);
+void Print(BLOCKINFO* bli, char* str);
+void PutCharacter(BLOCKINFO* bli, unsigned int a, unsigned int b, unsigned int FontSize);
+void MakeSizedPixel(BLOCKINFO* bli, const unsigned int a, const unsigned int b, const unsigned int f);
+void MakeRectangle(BLOCKINFO* bli, unsigned int a, unsigned int b, unsigned int w, unsigned int h, unsigned int c);
 
 void main(BLOCKINFO* bi)
 {
-
-	SetCharacter(0, 0, bi, ORANGE, 5);
+    MakeRectangle(bi, 1, 1, 100, 100, DARKORANGE);
+	// MakeSizedPixel(bi, 1, 1, 10);
+	//Print(bi, "NA");
 	
 	while(1){__asm__ ("hlt");}
 }
 
-void SetCharacter(unsigned int a, unsigned int b, BLOCKINFO* bli, unsigned int c, unsigned int FontSize)
+void Print(BLOCKINFO* bli, char* str)
+{
+	PutCharacter(bli, 0, 0, 5);
+}
+
+void PutCharacter(BLOCKINFO* bli, unsigned int a, unsigned int b, unsigned int FontSize)
 {
 	unsigned int x = a;
-	unsigned int temp = x;
 	unsigned int y = b;
+	unsigned int temp = x;
 	for(unsigned int fc = 0; fc < 128; fc++)
 	{
 		unsigned int asci = asciifont[fc];
@@ -74,12 +90,12 @@ void SetCharacter(unsigned int a, unsigned int b, BLOCKINFO* bli, unsigned int c
 			}
 			case 1:
 			{
-				MakeRectangle(x, y, FontSize, FontSize, bli, c);
+				MakeSizedPixel(bli, x, y, 5);
 				break;
 			}
 		}
 		x += FontSize;
-		if(x > (FontSize * 8))
+		if(x > (FontSize * 7))
 		{
 			x = temp;
 			y += FontSize;
@@ -87,10 +103,24 @@ void SetCharacter(unsigned int a, unsigned int b, BLOCKINFO* bli, unsigned int c
 	}
 }
 
-void MakeRectangle(unsigned int a, unsigned int b, unsigned int w, unsigned int h, BLOCKINFO* bli, unsigned int c)
+void MakeSizedPixel(BLOCKINFO* bli, const unsigned int a, const unsigned int b, const unsigned int f)
 {
-	unsigned int width = a + w;
-	unsigned int height = b + w;
+	unsigned int fs1 = f + a;
+	unsigned int fs2 = f + b;
+
+	for(unsigned int y = b; y < fs2; y++)
+    {
+        for(unsigned int x = a; x < fs1; x++)
+        {
+            *(unsigned int*)(x + (y * bli->PixelsPerScanLine) + (unsigned int*)(bli->BaseAddress)) = CYAN;
+        }
+    }
+}
+
+void MakeRectangle(BLOCKINFO* bli, unsigned int a, unsigned int b, unsigned int w, unsigned int h, unsigned int c)
+{
+	unsigned int width = (w + a);
+	unsigned int height = (h + b);
 	
 	for(unsigned int y = b; y < height; y++)
     {
@@ -100,4 +130,5 @@ void MakeRectangle(unsigned int a, unsigned int b, unsigned int w, unsigned int 
         }
     }
 }
+
 // __asm__ __volatile__
