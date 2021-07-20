fccd041013a0187a424994dfaec61301fd91e43f
ThatOSDev <starlordgo@protonmail.com>
2021-07-20 10:00:38 -0400

    Progress Bish !

--- src/code3/compile.bat
+++ src/code3/compile.bat
@@ -1,3 +1,3 @@
-gcc -Wall -Werror -Wno-unused-variable -m64 -Wl,--oformat=binary -e tmain -c loader.c -o loader.bin
+gcc -Wall -Werror -Wno-unused-variable -std=c99 -O0 -nostdinc -nostdlib -ffreestanding -m64 -Wl,--oformat=binary -e tmain -c loader.c -o loader.bin
 
 pause
