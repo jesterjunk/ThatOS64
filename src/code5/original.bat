8cfd52480165ba25bdf3aa83eec4a069df5c0ec4
ThatOSDev <starlordgo@protonmail.com>
2021-08-14 07:20:57 -0400

    Removed the GDT from loader in favor of the EFI loading the GDT

--- src/code5/original.bat
+++ src/code5/original.bat
@@ -1,7 +1,3 @@
-cd gdt
-fasm gdt.asm
-cd ..
-
 gcc -Wall -Werror -Wno-unused-variable -std=c99 -O0 -nostdinc -nostdlib -ffreestanding -m64 -Wl,--oformat=binary -e main -c loader.c -o loader.bin
 
 pause
